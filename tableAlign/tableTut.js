276365e73d459875c1568b04b4ce5d9aea802d8b
hugoresende27 <hugoresende27@gmail.com>
2022-08-21 09:19:22 +0100

    table sort2

--- tableAlign/tableTut.js
+++ tableAlign/tableTut.js
@@ -2,16 +2,24 @@ $("document").ready(function(){
 
     $("th").each(function(column){
         $(this).hover(function(){
+            $(this).data("type", $(this).attr("class"));
             $(this).addClass("clickable");
         }, function(){
             $(this).removeClass("clickable");
         });
 
         $(this).click(function(){
+            let type = $(this).data("type");
             let records = $("table").find("tbody > tr");
             records.sort(function (a,b){
                 let value1 = $(a).children("td").eq(column).text();
                 let value2 = $(b).children("td").eq(column).text();
+
+                if (type == "num")
+                {
+                    value1 *= 1; //convert string to number
+                    value2 *= 1; //convert string to number
+                }
 
                 return (value1 < value2) ? -1 : (value1 > value2 ? 1 : 0 );
             });
@@ -24,4 +32,4 @@ $("document").ready(function(){
 });
 
 //https://www.youtube.com/watch?v=7XuIZBLOF7A
-//https://youtu.be/Dg9FCdEKymc?t=178
+    //https://youtu.be/Dg9FCdEKymc?t=178
